d1aa125438c14bc7adb783f651ea222d9f1cf02a
boehis <sboehi@me.com>
2017-05-14 21:13:38 +0200

    admin area

--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,48 @@
 (function() {
 
-    var app = angular.module("myApp", ["ngRoute"])
+  var app = angular.module("myApp", ["ngRoute"])
 
-        .controller('PageController', function($scope, $route, $location) {
-            for (link of $(".link a")) {
-                if (link.getAttribute("href") == ("#" + $location.$$path)) {
-                    link.parentElement.classList.add("active")
-                }else {
-                  link.parentElement.classList.remove("active")
-                }
-            }
-        })
+    .controller('PageController', function($scope, $route, $location) {
+      for (link of $(".link a")) {
+        if (link.getAttribute("href") == ("#" + $location.$$path)) {
+          link.parentElement.classList.add("active")
+        } else {
+          link.parentElement.classList.remove("active")
+        }
+      }
+    })
+    .controller('ProgramController', function($scope, $route, $location, $http) {
+      $http({
+        method: 'GET',
+        url: '../admin/program.json'
+      }).then(function successCallback(response) {
+        $scope.program = response.data;
+      }, function errorCallback(response) {
+        $scope.error = response.statusText;
+      });
+    })
 
-        .config(function($routeProvider, $locationProvider) {
-            $routeProvider
-                .when("/", {
-                    templateUrl: "templates/main.htm",
-                    controller: 'PageController'
-                })
-                .when("/program", {
-                    templateUrl: "templates/program.htm",
-                    controller: 'PageController'
-                })
-                .when("/join", {
-                    templateUrl: "templates/join.htm",
-                    controller: 'PageController'
-                })
-                .when("/people", {
-                    templateUrl: "templates/people.htm",
-                    controller: 'PageController'
-                })
-                .otherwise({
-                    redirectTo: '/'
-                });
+    .config(function($routeProvider, $locationProvider) {
+      $routeProvider
+        .when("/", {
+          templateUrl: "templates/main.htm",
+          controller: 'PageController'
         })
+        .when("/program", {
+          templateUrl: "templates/program.htm",
+          controller: 'ProgramController'
+        })
+        .when("/join", {
+          templateUrl: "templates/join.htm",
+          controller: 'PageController'
+        })
+        .when("/people", {
+          templateUrl: "templates/people.htm",
+          controller: 'PageController'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    })
 
 })();
